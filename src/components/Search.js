8a7e64600f3677e6e938def8295619b83a1904ca
sanjaybora15 <sanjaybora15@gmail.com>
2020-01-08 02:14:10 +0530

    search component updated and removed some code

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,61 +1,102 @@
-import React from 'react';
-import Searchbar from './Searchbar';
-import newsApi from '../api/newsApi';
-import {Redirect} from 'react-router-dom'
+import React from "react";
+import Searchbar from "./Searchbar";
+import newsApi from "../api/newsApi";
+import { Redirect } from "react-router-dom";
 // import Spinner from './Spinner'
 
-class App extends React.Component{
-  constructor( props ) {
-    super( props );
-    this.state={
-      searchList:[],
-      searchClicked:false,
-      value:'',
-      currentPageNo:1,
-      loading:true
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchList: {},
+      searchClicked: false,
+      value: "",
+      currentPageNo: 1,
+      loading: true
+    };
   }
   componentDidMount() {
-    let searchValue = window.location.search.substr(3)
-    if(searchValue){
-      this.onSearchSubmit(searchValue,1)
+    let searchValue = window.location.search.substr(3);
+    if (searchValue) {
+      this.onSearchSubmit(searchValue, 1);
     }
-    
   }
 
-  onSearchSubmit = async (value,pageNo) => {
-      this.setState({loading:true})
-        const response = await newsApi.get(`everything?q=${value}&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=${pageNo}`,{
-        params:{query:value}
-    })
-    if(response.data.status === 'ok'){
+  onSearchSubmit = async (value, pageNo) => {
+    this.setState({ loading: true });
+    const response = await newsApi.get(
+      `everything?q=${value}&apiKey=363d26dd3d664d199ca63adc371e22aa&pageSize=10&page=${pageNo}`,
+      {
+        params: { query: value }
+      }
+    );
+    if (response.data.status === "ok") {
       this.setState({
-        searchList:response.data.articles,
-        value:value,
-        searchClicked:true,
-        loading:false
-      })
-  }}
+        searchList: response.data,
+        result: response.data,
+        value: value,
+        currentPageNo: pageNo,
+        searchClicked: true,
+        loading: false
+      });
+    }
+  };
 
-  handlePageClick = (e) => {
+  handleLoadMoreNews = type => {
+    console.log(type);
+    if (type === "next") {
+      this.setState(
+        prevState => ({
+          currentPageNo: prevState.currentPageNo + 1
+        }),
+        () => {
+          this.onSearchSubmit(this.state.value, this.state.currentPageNo);
+        }
+      );
+    } else {
+      this.setState(
+        prevState => ({
+          currentPageNo: prevState.currentPageNo - 1
+        }),
+        () => {
+          this.onSearchSubmit(this.state.value, this.state.currentPageNo);
+        }
+      );
+    }
+  };
 
-  }
-      
+  render() {
+    const {
+      value,
+      searchClicked,
+      searchList,
+      loading,
+      currentPageNo
+    } = this.state;
 
-  render(){
-    const {value,searchClicked,searchList,loading} = this.state
+    let rediret = searchClicked ? (
+      <Redirect
+        to={{
+          pathname: `/search?q=${value}`,
+          handleLoadMore: this.handleLoadMoreNews,
+          state: {
+            searchList: searchList,
+            loading: loading,
+            currentPageNo: currentPageNo
+          }
+        }}
+      />
+    ) : (
+      ""
+    );
 
-    let rediret = (
-        searchClicked ?<Redirect to={{pathname:`/search?q=${value}`,
-        state:{searchList:searchList,loading:loading}}} /> : "")
-
-    return(
+    return (
       <React.Fragment>
-          <Searchbar onSubmit={this.onSearchSubmit} />
-          {rediret}
+        <Searchbar onSubmit={this.onSearchSubmit} />
+        {rediret}
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default App
+export default App;
